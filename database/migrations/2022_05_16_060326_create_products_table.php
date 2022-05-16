3988b24f33a518789792b47072d6eb90a78b72ac
yutaisano <yuta.isano@gmail.com>
2022-05-16 15:09:13 +0900

    商品一覧作成

--- database/migrations/2022_05_16_060326_create_products_table.php
+++ database/migrations/2022_05_16_060326_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('img_path');
             $table->string('商品名');
             $table->integer('価格');
             $table->integer('在庫数');
-            $table->string('メーカー名');
+            $table->string('コメント');
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateProductTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('products');
     }
 }
